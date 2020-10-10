42d9c8bf0da1a6cdc633a3c40e34904674ed127b
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-10-10 13:19:37 -0400

    Replace box with placeholder balloon

--- project/src/Obstacle.gd
+++ project/src/Obstacle.gd
@@ -2,16 +2,10 @@ extends Area2D
 
 signal dead(body)
 
-export var slidespeed := 100
-
 
 func _process(delta):
 	if position.x < -50:
 		queue_free()
-
-
-func _draw():
-	draw_rect(Rect2(Vector2.ZERO, $CollisionShape2D.shape.extents*2), Color.black)
 
 
 func _on_Obstacle_body_entered(body):
