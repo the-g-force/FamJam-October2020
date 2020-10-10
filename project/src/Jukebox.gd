3f953cff17ee7f7dac6be45ec9fef9350e58fbbf
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-10-10 12:21:16 -0400

    Separate music and sfx buses

--- project/src/Jukebox.gd
+++ project/src/Jukebox.gd
@@ -2,4 +2,5 @@ extends AudioStreamPlayer
 
 
 func toggle_mute() -> void:
-	AudioServer.set_bus_mute(0, not AudioServer.is_bus_mute(0))
+	var music_bus := AudioServer.get_bus_index("Music")
+	AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
