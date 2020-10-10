d562b46ae470b7d9fd40ee4aaa552b6ae714b877
Alex Gestwicki <alex.gestwicki@gmail.com>
2020-10-10 09:38:33 -0400

    Add scolling clouds

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -2,11 +2,17 @@ extends Node2D
 
 signal dead
 
+var slidespeed := 100.0
+
 onready var player := $Plane
 
 func _restart():
 	player.position = Vector2(100, 150)
 	player.rotation_degrees = 0
+
+
+func _process(delta):
+	$ParallaxBackground.scroll_offset.x -= slidespeed/2.0 * delta
 
 
 func _on_Wall1_body_entered(body):
