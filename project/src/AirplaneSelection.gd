03cb19b10ebae359e92e79b031c137f192ded8f5
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-10-10 11:51:53 -0400

    Show all planes in the selection screen

--- project/src/AirplaneSelection.gd
+++ project/src/AirplaneSelection.gd
@@ -1,10 +1,20 @@
 extends Control
 
 signal airplane_selected(texture)
+tool
 
 var _images := [
 	preload("res://assets/Planes/b-29-D.png"),
-	preload("res://assets/Planes/plane-Leo.png")
+	preload("res://assets/Planes/biplane1.png"),
+	preload("res://assets/Planes/biplane2.png"),
+	preload("res://assets/Planes/jet1.png"),
+	preload("res://assets/Planes/jet2.png"),
+	preload("res://assets/Planes/jet3.png"),
+	preload("res://assets/Planes/passenger.png"),
+	preload("res://assets/Planes/passenger2.png"),
+	preload("res://assets/Planes/plane-D.png"),
+	preload("res://assets/Planes/plane-Leo.png"),
+	preload("res://assets/Planes/plane0D.png")
 ]
 
 onready var _button_container := $VBoxContainer/ButtonContainer
