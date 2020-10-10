9841f59114d64891209626131a3f784a511ecf8e
Paul Gestwicki <pvgestwicki@bsu.edu>
2020-10-10 13:44:42 -0400

    Integrate splash screen

---
+++ project/src/SplashScreen.gd
@@ -0,0 +1,15 @@
+extends Node
+
+const Game := preload("res://src/Level.tscn")
+
+var _touched := false
+
+func _input(event):
+	if not _touched and event is InputEventMouseButton and event.is_pressed():
+		$AnimationPlayer.play("FadeToBlack")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "FadeToBlack":
+		var game := Game.instance()
+		var _ignored := get_tree().change_scene_to(Game)
